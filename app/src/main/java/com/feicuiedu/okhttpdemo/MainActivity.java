7166b5e013f3d1af20e75f38db9bd4a8f80d826a
dailiwang <758017188@qq.com>
2016-08-17 15:24:37 +0800

    first commit

---
+++ app/src/main/java/com/feicuiedu/okhttpdemo/MainActivity.java
@@ -0,0 +1,48 @@
+package com.feicuiedu.okhttpdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.feicuiedu.okhttpdemo.demo.DemoAOkHttpGet;
+import com.feicuiedu.okhttpdemo.demo.DemoBOkHttpPost;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnItemClick;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.listView) ListView listView;
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 7.0 ButterKnife
+        ButterKnife.bind(this);
+
+        String[] datas = {"OkHttpGet","OkHttpPost"};
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datas);
+        listView.setAdapter(adapter);
+    }
+
+    @OnItemClick(R.id.listView)
+    public void itemClick(int position) {
+        Intent intent = new Intent();
+        switch (position) {
+            case 0:
+                intent.setClass(this, DemoAOkHttpGet.class);
+                startActivity(intent);
+                break;
+            case 1:
+                intent.setClass(this, DemoBOkHttpPost.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
